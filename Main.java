fcb3bfc63bcbb751ef6659f71a651402a4b506b6
NATHALIA DOMINGUES FREITAS <146946721+NathaliaDomingues@users.noreply.github.com>
2025-07-29 15:19:16 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,19 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         //INFORMAÇÕES DE UM CARRO
-       Carro carro = new Carro();
+        Carro carro = new Carro();
 
-       carro.nome = "Fusca";
-       carro.marca = "Volkswagen";
-       carro.anoDeLancamento = 1938;
-       carro.ipvaPago = "Não é mais cobrado no Brasil";
+        carro.nome = "Fusca";
+        carro.marca = "Volkswagen";
+        carro.anoDeLancamento = 1938;
+        carro.cor = "Azul marinho";
 
-       carro.exibeFichaTecnica();
-       carro.avaliacao(9);
-       carro.avaliacao(3);
-       carro.avaliacao(10);
-       System.out.println(carro.somaDasAvaliacoes);
-       System.out.println(carro.totalDeAvaliacoes);
-       System.out.println(carro.media());
+        carro.exibeFichaTecnica();
+        carro.avaliacao(9);
+        carro.avaliacao(3);
+        carro.avaliacao(10);
+        System.out.println("Soma das avaliações: " + carro.somaDasAvaliacoes);
+        System.out.println("Total das avaliações: " + carro.totalDeAvaliacoes);
+        System.out.println("Média da avaliações: " + carro.media());
     }
 }
