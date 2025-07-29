99622f8f2ba6783af971ef22f691c5d7d0870043
NATHALIA DOMINGUES FREITAS <146946721+NathaliaDomingues@users.noreply.github.com>
2025-07-29 15:09:30 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,27 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-//        Nome minhaFicha = new Nome();
-//
-//        minhaFicha.nome = "Nathalia";
-//        minhaFicha.corFavrita = "Amarelo";
-//        minhaFicha.comidaFavorita = "Pizza";
-//
-//        minhaFicha.exibeMinhaFicha();
+       Carro carro = new Carro();
 
+       carro.nome = "Fusca";
+       carro.marca = "Volkswagen";
+       carro.anoDeLancamento = 1938;
+       carro.ipvaPago = "Não é mais cobrado no Brasil";
 
-//        Carro carro = new Carro();
-//
-//        carro.nome = "Fusca";
-//        carro.marca = "Volkswagen";
-//        carro.anoDeLancamento = 1938;
-//        carro.ipvaPago = "Não é mais cobrado no Brasil";
-//
-//        carro.exibeFichaTecnica();
-//        carro.avaliacao(9);
-//        carro.avaliacao(3);
-//        carro.avaliacao(10);
-//        System.out.println(carro.somaDasAvaliacoes);
-//        System.out.println(carro.totalDeAvaliacoes);
-//        System.out.println(carro.media());
+       carro.exibeFichaTecnica();
+       carro.avaliacao(9);
+       carro.avaliacao(3);
+       carro.avaliacao(10);
+       System.out.println(carro.somaDasAvaliacoes);
+       System.out.println(carro.totalDeAvaliacoes);
+       System.out.println(carro.media());
     }
 }
